7a205acc0658ad625a7e49b4b1ab7ae90d6c5f03
Harry Hayden <h_man1992@hotmail.com>
2023-02-01 14:16:34 +0000

    add log in and log out. added method field to show is_owner

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,10 +5,16 @@ from .models import Profile
 # Serializer takend and modified from walkthrough project
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'about', 'maker_or_eater', 'image', 'favourite_pizza',
+            'is_owner'
         ]
